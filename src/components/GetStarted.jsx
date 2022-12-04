512eb847442b026bd5d41990f759219a2f807b16
unknown <haidaomar663@gmail.com>
2022-12-04 13:06:13 +0100

    edit in Hero page

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -6,10 +6,10 @@ const GetStarted = () => (
     bg-blue-gradient p-[2px] cursor-pointer`}>
         <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
             <div  className={`${styles.flexStart} flex-row`}>
-                <p className="font-poppins font-medium text-[18px] leading-[23px]">
+                <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
                     <span className="text-gradient">Get</span>
-                    <img src={arrowUp} alt="arrowUp" className="w-[23px] h-[23px] object-contain" />
                 </p>
+                <img src={arrowUp} alt="arrowUp" className="w-[23px] h-[23px] object-contain" />
             </div>
             <p className="font-poppins font-medium text-[18px] leading-[23px]">
             <span className="text-gradient">Started</span>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,13 +21,16 @@ const Hero = () => (
           ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'
         >
           The Next <br className='sm:block hidden'/> { " " }
-          <span className='text-gradient'>Generation</span> <br className='sm:block hidden'/> { " " }
-          Payment Method.
+          <span className='text-gradient'>Generation</span> { " " }
         </h1>
         <div className='ss:flex hidden md:mr-4 mr-0'>
           <GetStarted />
         </div>
       </div>
+      <h1  className='font-poppins font-semibold 
+      ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
+        Payment Method.
+      </h1>
     </div>
   </section>
 ) 
