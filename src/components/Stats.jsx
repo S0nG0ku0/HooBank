3a3dab4fa8d6fcb2253b19416f7151a5544d12ff
unknown <haidaomar663@gmail.com>
2022-12-05 13:38:37 +0100

    css

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -4,7 +4,7 @@ import styles from '../style'
 const Stats = () => (
     <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
       {stats.map( (stat) => (
-        <div key={stat.id} className={`flex-1 flex justify-start items-center flex-row m-3`}>
+        <div key={stat.id} className={`flex-1 flex justify-center items-center flex-row m-3`}>
           <h4 
             className='font-poppins font-semibold xs:text-[40px] 
             text-[30px] xs:leading-[53px] leading-[43px] text-white'>
