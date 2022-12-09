763fed31064218acc05f65287e2b7c054e40310e
unknown <haidaomar663@gmail.com>
2022-12-09 12:48:00 +0100

    add clients and CTA pages

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,13 +1,17 @@
-import { quotes } from '../assets';
-import { feedback } from '../constants';
-import styles, { layout } from '../style';
+import { clients } from "../constants";
+import styles from "../style";
 
-const Clients = () => {
-  return (
-    <section className={layout.section}>
-      
-    </section>
-  )
-}
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map( client => (
+        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+          <img src={client.logo} alt={`client ${client.id}`} 
+            className='sm:w-[192px] w-[100px] object-contain' />
+        </div>
+      ))}
+    </div>
+  </section>
+)
 
 export default Clients
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import styles from "../style";
+import { logo } from "../assets";
+import { footerLinks, socialMedia } from '../constants';
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () => (
+  <div>Footer</div>
+)
 
 export default Footer
